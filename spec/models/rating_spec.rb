20cd92ee45538c010bf1a35fa1186486b8a0a66e
Phil Lee <prodigy7748g@gmail.com>
2021-02-13 22:25:31 +0800

    add validates for score of rating

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
+
   it 'valid if score is between 0 and 10' do
     rating = Rating.new(score: 10)
     rating.valid?
     expect(rating.errors[:score]).to be_empty
   end
 
+  it 'invalid if score is less than 0' do
+    rating = Rating.new(score: -1)
+    rating.valid?
+    expect(rating.errors[:score]).not_to be_empty
+  end
 end
