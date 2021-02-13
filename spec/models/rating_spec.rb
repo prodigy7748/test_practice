4ed9f7ec703a76950269947cb994c5c2fc9b9e08
Phil Lee <prodigy7748g@gmail.com>
2021-02-13 22:29:40 +0800

    done validation test for score of rating

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -3,13 +3,21 @@ require 'rails_helper'
 RSpec.describe Rating, type: :model do
 
   it 'valid if score is between 0 and 10' do
-    rating = Rating.new(score: 10)
-    rating.valid?
-    expect(rating.errors[:score]).to be_empty
+    0.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      rating.valid?
+      expect(rating.errors[:score]).to be_empty
+    end
   end
 
   it 'invalid if score is less than 0' do
     rating = Rating.new(score: -1)
+    rating.valid?
+    expect(rating.errors[:score]).not_to be_empty
+  end
+
+  it 'invalid if score is greater than 10' do
+    rating = Rating.new(score: 11)
     rating.valid?
     expect(rating.errors[:score]).not_to be_empty
   end
