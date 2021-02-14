1fb81032c1c6856137ee9a1216c8cfec92ff80d7
Phil Lee <prodigy7748g@gmail.com>
2021-02-14 23:08:31 +0800

    seperate login and logout status test

--- spec/controllers/projects_controller.rb
+++ spec/controllers/projects_controller.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController do
-  it 'can get index' do
-    get :index
-    expect(response).to have_http_status(:success)
+  describe 'without login' do
+    it 'can get index without login' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'can not get new without login' do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 
-  it 'can get new' do
+  describe 'after login' do
     user = FactoryBot.create(:user)
-    sign_in user
-    get :new
-    expect(response).to have_http_status(:success)
+    let(:user) { sign_in user }
+
+    it 'can get new when login' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
   end
 end
